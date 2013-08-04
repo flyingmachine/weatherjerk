ab50ae781c00092e9f08ac31acf0e44dc42a2818
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-08-03 20:00:26 -0400

    getting api data

--- server/src/weatherjerk/lib/source-api.clj
+++ server/src/weatherjerk/lib/source-api.clj
@@ -1,8 +1,19 @@
 (ns weatherjerk.lib.source-api
-  (:require [org.httpkit.client :as http]))
+  (:require [org.httpkit.client :as http]
+            [weatherjerk.config :refer [config]]
+            [clojure.data.json :as json]))
 
 (def url "http://api.worldweatheronline.com/free/v1/weather.ashx")
+(defn options
+  [params]
+  {:timeout 200
+   :query-params (merge {:key (config :weather-api-key)
+                         :format "json"}
+                        params)})
 
 (defn location-forecast
   [location]
-  )
+  (-> @(http/get url (options {:q location}))
+      :body
+      json/read-str
+      (get "data")))
