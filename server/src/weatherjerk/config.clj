ecec5ac643532289abfa204a4f18af7e76125ad2
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-11-18 23:01:04 -0500

    no datomic

--- server/src/weatherjerk/config.clj
+++ server/src/weatherjerk/config.clj
@@ -4,8 +4,6 @@
 (def conf {:html-paths ["html-app"
                         "../html-app/app"
                         "../html-app/.tmp"]
-           :datomic {:db-uri "datomic:free://localhost:4334/gp2"
-                     :test-uri "datomic:mem://gp2"}
            :weather-api-key (env :weather-api-key)})
 
 (defn config
